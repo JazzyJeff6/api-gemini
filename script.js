4a91c2fe17749c7bd3dc09d862d2c0f1d573dbf3
Igor <delmasquio3@gmail.com>
2025-07-11 02:17:58 -0300

    Projeto integrado com IA Gemini

---
+++ script.js
@@ -0,0 +1,108 @@
+const apikeyInput = document.getElementById('apiKey')
+const gameSelect = document.getElementById('gameSelect')
+const questionInput = document.getElementById('questionInput')
+const askButton = document.getElementById('askButton')
+const aiResponse = document.getElementById('aiResponse')
+const form = document.getElementById('form')
+
+const markdownToHTML = (text) => {
+    const converter = new showdown.Converter()
+    return converter.makeHtml(text)
+}
+
+const perguntarIA = async (question, game, apiKey) => {
+    const model = 'gemini-2.5-flash'
+    const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
+    const pergunta = `
+        ## Especialidade
+        Você é um especialista em League of Legends, com conhecimento atualizado do meta atual e dos patches mais recentes. ${game}
+
+        ## Tarefa
+        Quero que me diga a melhor build possível para o seguinte campeão:
+
+        ## Regras
+        Campeão: [NOME DO CAMPEÃO]
+
+        Rota: [TOP / MID / ADC / SUP / JG]
+
+        Estilo de jogo: [agressivo / seguro / scaling / split push / roaming etc]
+
+        Composição inimiga: [Ex.: muito AP / muito CC / campeões móveis etc]
+        - Considere a data atual ${new Date().toLocaleDateString()}
+        - Faça pesquisas atualizadas sobre o patch atual, baseado na data atual, para dar uma resposta coernete.
+        - Nunca responda itens que Você não tenha certeza que exista no patch atual.
+
+        - Responda com no maximo 1000 caracteres
+
+        ## Resposta
+        Melhor ordem de habilidades
+
+        Melhores runas (e por quê)
+
+        Itens iniciais, principais e situacionais (explicando as escolhas)
+
+        Dicas estratégicas com base na build
+
+        Qualquer ajuste importante com base no patch atual (se souber)  
+
+        ---
+
+        Aqui esta a pergunta do Usuário: ${question}
+    `
+    const contents = [{
+        role: 'user',
+        parts: [{
+            text: pergunta
+        }]
+    }]
+
+    const tools = [{
+        google_search:{}
+    }
+    ]
+
+    const response = await fetch(geminiURL, {
+        method: `POST`,
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            contents,
+            tools
+        })
+    })
+
+    const data= await response.json()
+    return data.candidates[0].content.parts[0].text
+}
+
+const enviarFormulario = async (event) => {
+    event.preventDefault()
+    const apiKey = apikeyInput.value
+    const game = gameSelect.value
+    const question = questionInput.value
+
+    if(apiKey == '' || game == '' || question == '') {
+        alert('Por favor preencha todos os campos')
+        return 
+    }
+
+    askButton.disabled = true
+    askButton.textContent = 'Perguntando...'
+    askButton.classList.add('loading')
+
+    try{
+      const text = await perguntarIA(question, game, apiKey)
+        aiResponse.querySelector('.response-content').innerHTML = markdownToHTML(text)
+        aiResponse.classList.remove('hidden')
+
+    } catch(error){
+        console.log('Erro: ', error)
+    } finally{
+        askButton.disabled = false
+        askButton.textContent = 'Perguntar'
+        askButton.classList.remove('loading')
+    }
+
+}
+form.addEventListener('submit', enviarFormulario)
